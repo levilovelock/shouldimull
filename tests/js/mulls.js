11d1e88e75f300192196d3df802eb6339cdeea08
Levi Lovelock <lovelock.levi@gmail.com>
2015-07-10 20:56:40 +1000

    Fixed some javascript tests

--- tests/js/mulls.js
+++ tests/js/mulls.js
@@ -25,7 +25,7 @@ QUnit.test("test draw 0 from 60 in 7 with 4, 10 turns", function(assert) {
     assert.ok(round(r) == results[i]);
   }
 
-  assert.ok(count == 10);
+  assert.ok(count === 10);
 });
 
 // ==============================================
@@ -34,27 +34,27 @@ QUnit.test("test draw 0 from 60 in 7 with 4, 10 turns", function(assert) {
 
 QUnit.test("test draw 7 from population of 60 with number of successes equal to 24, wanting exactly 3", function(assert){
   r = calcExactHypergeometricFormula(3,60,7,24);
-  assert.ok(round(r) == 30.87);
+  assert.ok(round(r) === "30.87");
 });
 
 QUnit.test("test draw 1 from population of 2 with number of successes equal to 2, wanting exactly 1", function(assert){
   r = calcExactHypergeometricFormula(1,2,1,2);
-  assert.ok(round(r) == 100.00);
+  assert.ok(round(r) === "100.00");
 });
 
 QUnit.test("test draw 2 from population of 2 with number of successes equal to 2, wanting exactly 2", function(assert){
   r = calcExactHypergeometricFormula(2,2,2,2);
-  assert.ok(round(r) == 100.00);
+  assert.ok(round(r) === "100.00");
 });
 
 QUnit.test("test draw 0 from population of 2 with number of successes equal to 2, wanting exactly 2", function(assert){
   r = calcExactHypergeometricFormula(2,2,0,2);
-  assert.ok(round(r) == 0.00);
+  assert.ok(round(r) === "0.00");
 });
 
 QUnit.test("test draw 7 from population of 60 with number of successes equal to 4, with 1 mulligan", function(assert){
   r = calcExactHypergeometricFormulaWithMulligans(1,60,7,4,1);
-  assert.ok(round(r) == 53.88);
+  assert.ok(round(r) === "53.88");
 });
 
 // ==============================================
@@ -63,18 +63,18 @@ QUnit.test("test draw 7 from population of 60 with number of successes equal to 
 
 QUnit.test("test draw 7 from population of 60 with number of successes equal to 4, wanting 1", function(assert){
   r = calcAtLeastHypergeometricFormula(1,60,7,4);
-  assert.ok(round(r) == 39.95);
+  assert.ok(round(r) === "39.95");
 });
 
 QUnit.test("test draw 24 from population of 133 with number of successes equal to 42, wanting 6", function(assert){
   r = calcAtLeastHypergeometricFormula(6,133,24,42);
-  assert.ok(round(r) == 84.35);
+  assert.ok(round(r) === "84.35");
 });
 
 QUnit.test("test draw 1 from population of 60 with number of successes equal to 1, wanting 1", function(assert){
   r1 = calcAtLeastHypergeometricFormula(1,60,1,4);
   r2 = calcExactHypergeometricFormula(1,60,1,4);
-  assert.ok(round(r1) === 6.67);
+  assert.ok(round(r1) === "6.67");
   assert.ok(r1 === r2);
 });
 
@@ -93,6 +93,6 @@ QUnit.test("test combinations", function(assert){
     var r = testMap[i][1]
     var expectedRes = testMap[i][2]
     res = combi(n,r);
-    assert.ok(res == expectedRes);
+    assert.ok(res === expectedRes);
   }
 });
